e07463ac43d129f6dc8fb5d28a2a86a893b3784f
Varun <varunpawardigital@gmail.com>
2025-05-01 05:43:43 +0530

    updated

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import styles from "../css/signup.module.css"; // Import the CSS file
 import { Link, useNavigate } from "react-router-dom";
 import api from "../api/axios";
